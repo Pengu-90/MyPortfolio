961ec2696881c36c1217c135a54394a82e399156
pengu-90 <mikedaguil64@gmail.com>
2024-08-30 08:14:44 +0800

    resume

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const resumeDL_btn = document.getElementById('resumeDL_btn');
 
  function downloadResume() {
-    location.href = "https://docs.google.com/uc?export=download&id=1RkG4c45CQLUZ_n_o0UvK7KFoLzNVGkwL";
+    location.href = "https://docs.google.com/uc?export=download&id=1OPOkRGVZiusXkJQulikhq2beaNA68qj9";
 };
